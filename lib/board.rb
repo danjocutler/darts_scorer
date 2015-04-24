d271eb82b51ff152deed097c41f918159b2888cf
danjocutler <danjocutler@gmail.com>
2015-04-24 17:14:07 +0100

    Singles, doubles and trebles added

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,20 @@
 class Board
 
-	attr_reader :scorer, :outer_bull, :inner_bull
+	attr_reader :number, :outer_bull, :inner_bull
 
 	def initialize
-		@scorer = [*1..20]
+		@number = [*1..20]
 		@outer_bull = 25
 		@inner_bull = 50
 	end
 
+	def score(number, multiplier)
+		if multiplier == "single"
+			number
+		elsif multiplier == "double"
+			number * 2
+		else
+			number * 3
+		end
+	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@ describe Board do
 	let(:board) {Board.new}
 
 	it "has numbers 1 - 20" do
-		expect(board.scorer.count).to equal(20)
+		expect(board.number.count).to equal(20)
 	end
 
 	it "has an outer bull worth 25 points" do
@@ -14,5 +14,11 @@ describe Board do
 	it "has an inner bull worth 50 points" do
 		expect(board.inner_bull).to equal(50)
 	end
+
+	it "each number has a single, double and treble value" do
+		expect(board.score(20, "single")).to equal(20)
+		expect(board.score(20, "double")).to equal(40)
+		expect(board.score(20, "treble")).to equal(60)
+	end	
 
 end
